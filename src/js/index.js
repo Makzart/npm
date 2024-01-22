a49be1ba57d840397e1079d10d5161ba3c17ac99
Makzart <max.artyukhov.14@gmail.com>
2024-01-22 02:03:38 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const sum = function funcSum(a, b) {
+  return a + b;
+};
+sum(1, 2);
+const minus = function funcMinus(a, b) {
+  return a - b;
+};
+minus(1, 2);
+const mul = function funcMul(a, b) {
+  return a * b;
+};
+mul(1, 2);
